c6244be9e6863bbf3292d527e24e957ead3282af
sreeramadasuarun <sreeramadasuarun@gmail.com>
2023-03-28 14:34:16 +0530

    first-comit

---
+++ src/Componets/userdata.js
@@ -0,0 +1,122 @@
+export default [
+  {
+    id: 1,
+    name: "K.G.F",
+    username: "kgf",
+    email: "kgf@april.biz",
+    imgurl:
+      "https://images.newindianexpress.com/uploads/user/imagelibrary/2022/4/13/w1200X800/KGF2_movie.jpg",
+  },
+  {
+    id: 2,
+    name: "ARUN",
+    username: "arun",
+    email: "arun@melissa.tv",
+    imgurl:
+      "https://mir-s3-cdn-cf.behance.net/user/230/a9d4fa70663677.5d331da30ee4b.jpg",
+  },
+  {
+    id: 3,
+    name: "TONY",
+    username: "tony",
+    email: "tony@yesenia.net",
+    imgurl:
+      "https://media1.popsugar-assets.com/files/thumbor/tS6c3oXAC7--Nm9XFJWzNWxGgFI/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/07/01/076/n/46207611/tmp_YF8Suz_217bdaf4519e21ea_MCDAVEN_EC254.jpg",
+  },
+  {
+    id: 4,
+    name: "K.G.F",
+    username: "kgf",
+    email: "kgf@april.biz",
+    imgurl:
+      "https://images.newindianexpress.com/uploads/user/imagelibrary/2022/4/13/w1200X800/KGF2_movie.jpg",
+  },
+  {
+    id: 5,
+    name: "ARUN",
+    username: "arun",
+    email: "arun@melissa.tv",
+    imgurl:
+      "https://mir-s3-cdn-cf.behance.net/user/230/a9d4fa70663677.5d331da30ee4b.jpg",
+  },
+  {
+    id: 6,
+    name: "TONY",
+    username: "tony",
+    email: "tony@yesenia.net",
+    imgurl:
+      "https://media1.popsugar-assets.com/files/thumbor/tS6c3oXAC7--Nm9XFJWzNWxGgFI/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/07/01/076/n/46207611/tmp_YF8Suz_217bdaf4519e21ea_MCDAVEN_EC254.jpg",
+  },
+  {
+    id: 7,
+    name: "K.G.F",
+    username: "kgf",
+    email: "kgf@april.biz",
+    imgurl:
+      "https://images.newindianexpress.com/uploads/user/imagelibrary/2022/4/13/w1200X800/KGF2_movie.jpg",
+  },
+  {
+    id: 8,
+    name: "ARUN",
+    username: "arun",
+    email: "arun@melissa.tv",
+    imgurl:
+      "https://mir-s3-cdn-cf.behance.net/user/230/a9d4fa70663677.5d331da30ee4b.jpg",
+  },
+  {
+    id: 9,
+    name: "TONY",
+    username: "tony",
+    email: "tony@yesenia.net",
+    imgurl:
+      "https://media1.popsugar-assets.com/files/thumbor/tS6c3oXAC7--Nm9XFJWzNWxGgFI/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/07/01/076/n/46207611/tmp_YF8Suz_217bdaf4519e21ea_MCDAVEN_EC254.jpg",
+  },
+  {
+    id: 10,
+    name: "K.G.F",
+    username: "kgf",
+    email: "kgf@april.biz",
+    imgurl:
+      "https://images.newindianexpress.com/uploads/user/imagelibrary/2022/4/13/w1200X800/KGF2_movie.jpg",
+  },
+  {
+    id: 11,
+    name: "ARUN",
+    username: "arun",
+    email: "arun@melissa.tv",
+    imgurl:
+      "https://mir-s3-cdn-cf.behance.net/user/230/a9d4fa70663677.5d331da30ee4b.jpg",
+  },
+  {
+    id: 12,
+    name: "TONY",
+    username: "tony",
+    email: "tony@yesenia.net",
+    imgurl:
+      "https://media1.popsugar-assets.com/files/thumbor/tS6c3oXAC7--Nm9XFJWzNWxGgFI/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/07/01/076/n/46207611/tmp_YF8Suz_217bdaf4519e21ea_MCDAVEN_EC254.jpg",
+  },
+  {
+    id: 13,
+    name: "K.G.F",
+    username: "kgf",
+    email: "kgf@april.biz",
+    imgurl:
+      "https://images.newindianexpress.com/uploads/user/imagelibrary/2022/4/13/w1200X800/KGF2_movie.jpg",
+  },
+  {
+    id: 14,
+    name: "ARUN",
+    username: "arun",
+    email: "arun@melissa.tv",
+    imgurl:
+      "https://mir-s3-cdn-cf.behance.net/user/230/a9d4fa70663677.5d331da30ee4b.jpg",
+  },
+  {
+    id: 15,
+    name: "TONY",
+    username: "tony",
+    email: "tony@yesenia.net",
+    imgurl:
+      "https://media1.popsugar-assets.com/files/thumbor/tS6c3oXAC7--Nm9XFJWzNWxGgFI/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/07/01/076/n/46207611/tmp_YF8Suz_217bdaf4519e21ea_MCDAVEN_EC254.jpg",
+  },
+];
---
+++ src/Componets/users.js
@@ -0,0 +1,40 @@
+import React from "react";
+import userdata from "./userdata";
+import "../Componets/usersstyles.css";
+
+const Users = () => {
+  return (
+    <div className="allcard">
+      <h1>Users</h1>
+      <div className="cardcon">
+        {userdata.map(({ imgurl, name, id, username, email }) => {
+          return (
+            <div key={id}>
+              <article className="card">
+                <div className="image">
+                  <img src={imgurl} alt="image" />
+                </div>
+                <h3>{name}</h3>
+
+                <div className="right-left">
+                  <h4>
+                    username:
+                    <br />
+                    {username}
+                  </h4>
+                  <h4>
+                    email:
+                    <br />
+                    {email}
+                  </h4>
+                </div>
+              </article>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Users;
